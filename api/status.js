2eae44f8ff765872962ee0c534b17591cd1d941e
gestoriagonzalez <gestoriayserviciosgonzalez@gmail.com>
2026-03-06 17:11:58 -0600

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,1 +1,14 @@
 
+export default async function handler(req, res) {
+
+const { id } = req.query
+
+const response = await fetch(
+`https://equations-rocket-annie-daughter.trycloudflare.com/api/actas/job/status/${id}`
+)
+
+const data = await response.json()
+
+res.status(200).json(data)
+
+}
