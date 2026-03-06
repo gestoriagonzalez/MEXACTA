5046e2404743a63ee873bf39543c2f071e95a066
gestoriagonzalez <gestoriayserviciosgonzalez@gmail.com>
2026-03-06 17:11:25 -0600

    Update new.js

--- api/new.js
+++ api/new.js
@@ -1,1 +1,19 @@
 
+export default async function handler(req, res) {
+
+const response = await fetch(
+"https://equations-rocket-annie-daughter.trycloudflare.com/api/actas/job/new",
+{
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify(req.body)
+}
+)
+
+const data = await response.json()
+
+res.status(200).json(data)
+
+}
